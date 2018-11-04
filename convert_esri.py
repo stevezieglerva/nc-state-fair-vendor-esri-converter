17802af06a8248913b8aecd8cf3d01892b67a827
stevezieglerva <stephen.v.ziegler@gmail.com>
2018-11-04 12:03:53 -0500

    More tests and main

--- convert_esri.py
+++ convert_esri.py
@@ -4,6 +4,7 @@ import csv
 import json
 
 
+
 NC_STATE_FAIR_ESRI_VENDOR_URL = "https://www.ncmhtd.com/arcgis/rest/services/AG_SF/StateFairFRFapp/MapServer/1/query?returnGeometry=true&where=1%3D1&outSr=4326&outFields=*&inSr=4326&geometry={%22xmin%22%3A-78.717041015625%2C%22ymin%22%3A35.79108281624994%2C%22xmax%22%3A-78.7060546875%2C%22ymax%22%3A35.79999392988527%2C%22spatialReference%22%3A{%22wkid%22%3A4326}}&geometryType=esriGeometryEnvelope&spatialRel=esriSpatialRelIntersects&geometryPrecision=6&f=json"
 
 
@@ -13,15 +14,52 @@ def get_esri_json(url):
 	return json_response
 
 
+def json_is_valid(input_json):
+	if "displayFieldName" not in input_json:
+		return False
+	if "features" not in input_json:
+		return False
+	else:
+		features = input_json["features"]
+		if len(features) == 0:
+			return False
+	return True
+
 def convert_esri_json_to_csv(input_json):
-	result = {}
+	csv_result = io.StringIO()
+	writer = csv.writer(csv_result, quoting=csv.QUOTE_NONNUMERIC)
 
 	count = 0
+	header = {}
+	header_list = []
 	for feature in input_json["features"]:
 		count = count + 1
 		print(count)
+		attributes = feature["attributes"]
+		geometry = feature["geometry"]
+		if count == 1:
+			header = sorted(attributes.keys())
+			header_list = list(header)
+			header_list.append("x")
+			header_list.append("y")
+			writer.writerow(header_list)
+		values_list = []
+		for column in header:
+			values_list.append(attributes[column])
+		values_list.append(geometry["x"])
+		values_list.append(geometry["y"])
+		writer.writerow(values_list)	
+	return csv_result.getvalue()
 
 
-	return result
+def main():
+	vendor_json = get_esri_json(NC_STATE_FAIR_ESRI_VENDOR_URL)
+	print("JSON Length:" + str(len(vendor_json)))
+	print("JSON start:" + vendor_json[0:50])
+	print(json.dumps(vendor_json, indent=3))
+	csv_results = convert_esri_json_to_csv(vendor_json)
+	print(csv_results)
 
+if __name__ == '__main__':
+	main()		
 
--- tests/unit_tests.py
+++ tests/unit_tests.py
@@ -2,24 +2,7 @@ import unittest
 from convert_esri import *
 import json
 
-
-
-class TestMethods(unittest.TestCase):
-	def test_get_esri_json__valid_url__more_than_one_row_returned(self):
-		# Arrange
-
-		# Act
-		result = get_esri_json(NC_STATE_FAIR_ESRI_VENDOR_URL)
-		#print(json.dumps(result, indent=3))
-
-		# Assert
-		self.assertGreater(len(result), 1)
-
-
-
-	def test_convert_esri_json_to_csv__super_simple__header_and_row_returned(self):
-		# Arrange
-		input = {
+SINGLE_FEATURE_JSON = {
 			"displayFieldName": "NAMELABEL",
 			"fieldAliases": {
 				"OBJECTID": "OBJECTID",
@@ -168,13 +151,92 @@ class TestMethods(unittest.TestCase):
 			}]
 		}
 
+NO_DISPLAY_NAME = {}
 
+NO_FEATURES = {"displayName" = "ziegler", "features" = []}
+
+
+class TestMethods(unittest.TestCase):
+	def test_get_esri_json__valid_url__more_than_one_row_returned(self):
+		# Arrange
 
 		# Act
-		result = convert_esri_json_to_csv(input)
+		result = get_esri_json(NC_STATE_FAIR_ESRI_VENDOR_URL)
+		#print(json.dumps(result, indent=3))
 
 		# Assert
-		self.assertEqual(len(result), 2, "Expected single header and data row.")
+		self.assertGreater(len(result), 1)
+
+
+	def test_is_json_valid__no_displayName__return_false(self):
+		# Arrange
+
+		# Act
+		result = json_is_valid(NO_DISPLAY_NAME)
+
+		# Assert
+		self.assertFalse(result)
+
+	def test_is_json_valid__no_features__return_false(self):
+		# Arrange
+
+		# Act
+		result = json_is_valid(NO_FEATURES)
+
+		# Assert
+		self.assertFalse(result)
+
+
+	def test_is_json_valid__valid_json__return_true(self):
+		# Arrange
+
+		# Act
+		result = json_is_valid(SINGLE_FEATURE_JSON)
+
+		# Assert
+		self.assertTrue(result)
+
+
+	def test_convert_esri_json_to_csv__single_feature__header_and_row_returned(self):
+		# Arrange
+
+		# Act
+		result = convert_esri_json_to_csv(SINGLE_FEATURE_JSON)
+
+		# Assert
+		result_lines = result.split("\n")
+		self.assertEqual(len(result_lines), 3, "Expected single header, data row, and final new line.")
+
+
+
+
+	def test_convert_esri_json_to_csv__single_feature__header_has_right_columns(self):
+		# Arrange
+
+		# Act
+		result = convert_esri_json_to_csv(SINGLE_FEATURE_JSON)
+		result_lines = result.split("\n")
+		header_row = result_lines[0]
+		header_row_columns = header_row.split(",")
+
+		# Assert
+		self.assertEqual(header_row_columns[0], "\"CONTACT\"")
+		self.assertEqual(header_row_columns[18], "\"VENDORTYPE\"")
+
+
+
+	def test_convert_esri_json_to_csv__single_feature__data_row_has_right_columns(self):
+		# Arrange
+
+		# Act
+		result = convert_esri_json_to_csv(SINGLE_FEATURE_JSON)
+		result_lines = result.split("\n")
+		first_row = result_lines[1]
+
+		# Assert
+		self.assertTrue("\"null\",\"null\",\"Davis Concessions\",\"20 X 29\"" in first_row)
+		self.assertTrue("\"\",-78.71098,35.795005" in first_row)
+		print(result)
 
 
 if __name__ == '__main__':
